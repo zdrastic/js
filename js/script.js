adaed8132f33da9a3cd8268e0e1c1be4c53f9871
drastic <zdrastic90@gmail.com>
2023-04-17 16:49:33 +0300

    Some changes made

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-let wrapper = document.getElementsByClassName("wrapper");
-console.log(wrapper)
-console.log(wrapper)
+let burger = document.getElementById("dropdown__button");
+let myBurger = burger[0];
+let dropDownButton = document.querySelector("#dropdown__button");
+let dropDownList = document.querySelector("#dropdown__list");
+
+dropDownButton.addEventListener("click", ()=> {
+	dropDownList.classList.toggle("activeDropDown");
+})
+
+/*myBurger.addEventListener("click", () => {
+	let popUp = document.getElementById("dropdown__list")
+	openPopUp(popUp);
+}, false);
+
+function openPopUp(p) {
+	p.classList.toggle("activeDropDown");
+}*/
+
+/*
+document.onclick = function(e) {
+	let popUp = document.getElementById("dropdown__list");
+	if (e.target == myBurger) {
+		popUp.classList.toggle("activeDropDown");
+		console.log(popUp);
+		console.log(typeof(popUp));
+	} else {
+		popUp.classList.remove("activeDropDown");
+		console.log(popUp);
+		console.log(typeof(popUp));
+	}
+}
+*/
